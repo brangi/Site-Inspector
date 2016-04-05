1b02e28c25d0a0b573b04b999570c0ccc6a2a540
Gibran Rodriguez <gibran@munchunited.com>
2016-04-05 21:46:03 +0300

    New: Http request, extract data and check for actual content/patterns out of the body
    TODO: resolve response time and logging

--- src/inspector.erl
+++ src/inspector.erl
@@ -15,10 +15,31 @@ get_all_sites(Device, Accum) ->
         
     end.
 
-
-
-
-
+check_content(Data, Req) ->
+    Patterns = binary:split(list_to_binary(Req), <<" ">>),
+    case binary:match(Data, Patterns,[]) of
+        {_Pat1,_Pat2} ->
+            found;
+        nomatch ->
+            not_found;
+        _->
+            not_found
+    end.
+            
+request(Site, Req) ->
+    Method = get,
+    URL = Site,
+    Headers = [],
+    ReqBody = <<>>,
+    Options = [{follow_redirect, true}, {max_redirect, 5}],
+    {ok, S, H, Ref} = hackney:request(Method, URL, Headers, ReqBody, Options),
+    {ok, Data} = hackney:body(Ref),
+    %%Check for the content requierments 
+    Check = check_content(Data, Req),
+    %Calcualte the response time, extract response time from server and compare with
+    %request timestamp
+    {<<"Date">>, Time} = lists:keyfind(<<"Date">>, 1, H),
+    {S,Check,Time}.
 
 schedule(Seconds)->
     InspectJob = {{once, Seconds},
