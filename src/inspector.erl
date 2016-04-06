405ebdac5309102b4f28bbe3ea70244aaa41aa5d
Gibran Rodriguez <gibran@munchunited.com>
2016-04-06 21:54:14 +0300

    Response time calculation, format fixes and other ajustments

--- src/inspector.erl
+++ src/inspector.erl
@@ -5,7 +5,23 @@
 readsites() ->
     {ok, Device} = file:open(?P, [read]),
     get_all_sites(Device, []).
-    
+
+get_response_time(TimeStartSeconds, ResponseDate) ->
+
+      ResponseDateData = string:tokens(ResponseDate, " "),
+      [_,_,_,_,ReponseTime,_] = ResponseDateData,
+      TimeList = string:tokens(ReponseTime, ":"),
+      [H,M,S] = TimeList,
+      TimeTerm = {list_to_integer(H),list_to_integer(M),list_to_integer(S)},
+      TimeResponseSeconds = calendar:time_to_seconds(TimeTerm),
+      
+      
+      FinalResponseTime = TimeStartSeconds - TimeResponseSeconds ,
+      MiliSecondsResponse = FinalResponseTime / 1000,
+      
+      float_to_list(MiliSecondsResponse,[{decimals, 4}]).
+
+
 get_all_sites(Device, Accum) ->
     case io:get_line(Device, "") of
         eof  -> file:close(Device), Accum;
@@ -13,6 +29,7 @@ get_all_sites(Device, Accum) ->
             get_all_sites(Device, Accum ++ string:tokens(Line, "\n"))
         
     end.
+
 
 check_content(Data, Req) ->
     Patterns = binary:split(list_to_binary(Req), <<" ">>),
@@ -26,7 +43,7 @@ check_content(Data, Req) ->
 find_date(List) ->
     case lists:keyfind(<<"Date">>, 1, List) of
         {<<"Date">>, Result} -> Result;
-        false -> <<"No date">>
+        false -> <<"Not available">>
     end.
 
 
@@ -40,9 +57,16 @@ resolve_inspection(Sec) ->
                 io:format("Checking ~s ~p~n", [Site, Response]), 
                 RestCode = " Respose code: " ++ integer_to_list(Item1),
                 CriteriaReq = " Critera requierment: " ++ atom_to_list(Item2),
+                ResponseTime = case Item3 of
+                                "Not available" ->
+                                    " Response time: " ++ Item3;
+                                _->
+                                    " Response time: " ++ Item3++ " seconds"
+                            end,
                 LogResult = "Checking " ++ Site ++ " - . Results: " ++ 
                             RestCode ++
-                            CriteriaReq ++ Item3,
+                            CriteriaReq ++ 
+                            ResponseTime,
                 io:format(Write, "~s~n", [LogResult])
         end, Sites),
         file:close(Write),
@@ -54,6 +78,9 @@ sec_to_mil(Sec)->
     Sec * 1000.
 
 request(Site, Req) ->
+    {_,TimeNow} = calendar:universal_time(),
+    TimeStartSeconds = calendar:time_to_seconds(TimeNow),
+
     Method = get,
     URL = Site,
     Headers = [],
@@ -65,8 +92,14 @@ request(Site, Req) ->
     Check = check_content(Data, Req),
     %Calcualte the response time, extract response time from server and compare with
     %request timestamp
-    _ReponseDate = find_date(H),
-    {S, Check, binary_to_list(_ReponseDate)}.
+    _ReponseDate = binary_to_list(find_date(H)),
+    Res = case _ReponseDate of
+        "Not available" ->
+            "Not available";
+        _->
+            get_response_time(TimeStartSeconds,_ReponseDate)
+    end,
+    {S, Check, Res}.
 
 run_inspection(Seconds)->
     InspectJob = {{once, Seconds},
